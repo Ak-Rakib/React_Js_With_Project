ad31214bb8d41b382ec6b134ec76e2514c56dde7
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-05-02 19:33:20 +0600

    create first component

---
+++ Tic_Tac_Toe_Games/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
